950f5305cfe733d2824707cbfef811d8ed94bf7d
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-06-25 19:21:10 +0400

    add ability to draw line with multiple touches

---
+++ TouchTracker/DrawView.swift
@@ -0,0 +1,77 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Vlad Akhpanov on 25.06.2023.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var currentLines = [NSValue: Line]()
+    var finishedLines = [Line]()
+    
+    override func draw(_ rect: CGRect) {
+        UIColor.black.setStroke()
+        for line in finishedLines {
+            stroke(line)
+        }
+        
+        
+        UIColor.red.setStroke()
+        for (_, line) in currentLines {
+            stroke(line)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            let newLine = Line(begin: location, end: location)
+            
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key] = newLine
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key]?.end = touch.location(in: self)
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        for touch in touches {
+            let key = NSValue(nonretainedObject: touch)
+            if var line = currentLines[key] {
+                line.end = touch.location(in: self)
+                finishedLines.append(line)
+                currentLines.removeValue(forKey: key)
+            }
+        }
+        setNeedsDisplay()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        currentLines.removeAll()
+        setNeedsDisplay()
+    }
+    
+    func stroke(_ line: Line) {
+        let path = UIBezierPath()
+        path.lineWidth = 10
+        path.lineCapStyle = .round
+        
+        path.move(to: line.begin)
+        path.addLine(to: line.end)
+        path.stroke()
+    }
+}
---
+++ TouchTracker/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Vlad Akhpanov on 25.06.2023.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
