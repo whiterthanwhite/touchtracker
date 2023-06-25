452ab825193ab970c4739271461169bda438ef45
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-06-25 18:44:20 +0400

    Initial Commit

---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Vlad Akhpanov on 25.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
